32ae0c0f4776d6b0e42eea24d1b4c137698315c8
doodoodoong <doo_doong@hanmail.net>
2024-04-07 14:46:07 +0900

    responsive correct

--- app/components/main/Introduce.js
+++ app/components/main/Introduce.js
@@ -8,13 +8,21 @@ export default function Introduce() {
     <div className="flex flex-row mt-44 justify-between ">
       <div className="flex flex-col gap-2">
         <div>
-          <text className={`${roboto.className} text-8xl`}>World Tour</text>
+          <text
+            className={`${roboto.className}sm:text-4xl lg:text-8xl text-2xl`}
+          >
+            World Tour
+          </text>
         </div>
         <div>
-          <text className={`${roboto.className} text-8xl`}>GPT</text>
+          <text
+            className={`${roboto.className} sm:text-4xl lg:text-8xl text-2xl`}
+          >
+            GPT
+          </text>
         </div>
         <div>
-          <text className="text-3xl text-gray-300">
+          <text className="sm:text-2xl lg:text-3xl text-xl text-gray-300">
             여러분의 세계 여행 계획을 도와드립니다 !
           </text>
         </div>
@@ -28,7 +36,7 @@ export default function Introduce() {
           </Link>
         </div>
       </div>
-      <div className="w-96 h-96">
+      <div className="sm:w-44 sm:h-44 w-64 h-64 lg:w-96 lg:h-96">
         <Image alt="photo" src={tourPic} className="rounded-lg" />
       </div>
       <div className="w-1"></div>
