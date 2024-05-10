9a5ec359ebc933b0494e0bc2c1f95e4cd0469c23
doodoodoong <doo_doong@hanmail.net>
2024-05-10 16:14:05 +0900

    tour ment

--- app/main/tour/page.js
+++ app/main/tour/page.js
@@ -9,10 +9,12 @@ export default function Tour() {
         gradient={
           "linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%)"
         }
-        title={"여행하고 싶은 도시의 이름을 작성하고 엔터를 눌러주세요"}
+        title={"세계 도시들의 여행장소를 추천 받으세요."}
       />
       <Input
-        placeholder={"조사하고 싶은 도시의 이름을 입력하고 엔터를 눌러주세요"}
+        placeholder={
+          "예:여수의 자연환경, 문화, 역사가 드러나는 여행장소 추천해줘."
+        }
       />
     </div>
   );
